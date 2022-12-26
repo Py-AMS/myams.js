da29f70e2f8591e3a904f909f3e9fd1f41457a5f
Thierry Florac <tflorac@ulthar.net>
2022-12-26 12:18:35 +0100

    Comments...

--- src/js/myams-core.js
+++ src/js/myams-core.js
@@ -1,7 +1,7 @@
 /**
  * MyAMS core features
  *
- * This script is used to build MyAMS mini-core-package.
+ * This script is used to build MyAMS core-package.
  *
  * This package only includes MyAMS core features, but not CSS or external modules
  * which can be loaded using MyAMS.require function.
